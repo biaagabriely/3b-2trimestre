4b7608c9f27bdc2c39bae05d91226ce79a091deb
biaagabriely <127507324+biaagabriely@users.noreply.github.com>
2024-06-27 11:22:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado  = document.querySelector(".texto-principal");
+
+const pergintas= [
+    {
+        enunciado: "Pergunta 1",
+        alternativasa: ["Alternativa 1", "ALternativa 2"],
+
+    },
+    { 
+        enunciado: "Pergunta 2",
+        alternativasa: ["Alternativa 1", "ALternativa 2"],
+
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativasa: ["Alternativa 1", "ALternativa 2"],
+
+    }
+
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPerguntas (){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPerguntas
+
+
